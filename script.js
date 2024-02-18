4507027c008eb6f0ba29252580047eb7854392f6
Logan Butler <LoganCButler@users.noreply.github.com>
2024-02-18 13:45:08 -0500

    Add idol and advantage icons

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
 				{
 					"name": "Soda",
 					"image": "./assets/Survivor-46-Soda-Thompson.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Kenzie",
 					"image": "./assets/Survivor-46-Kenzie-Petty.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Randen",
 					"image": "./assets/Survivor-46-Randen-Montalvo.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Tevin",
 					"image": "./assets/Survivor-46-Tevin-Davis.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 			]
 		},
@@ -31,22 +39,30 @@ document.addEventListener("DOMContentLoaded", function () {
 				{
 					"name": "Jess",
 					"image": "./assets/Survivor-46-Jessica-Jess-Chong.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Charlie",
 					"image": "./assets/Survivor-46-Charlie-Davis.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Hunter",
 					"image": "./assets/Survivor-46-Hunter-McKnight.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Ben",
 					"image": "./assets/Survivor-46-Ben-Katzman.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 			]
 		},
@@ -56,22 +72,30 @@ document.addEventListener("DOMContentLoaded", function () {
 				{
 					"name": "Tiffany",
 					"image": "./assets/Survivor-46-Tiffany-Nicole-Ervin.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Venus",
 					"image": "./assets/Survivor-46-Venus-Vafa.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Ben",
 					"image": "./assets/Survivor-46-Ben-Katzman.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Liz",
 					"image": "./assets/Survivor-46-Liz-Wilcox.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 			]
 		},
@@ -81,22 +105,30 @@ document.addEventListener("DOMContentLoaded", function () {
 				{
 					"name": "Q",
 					"image": "./assets/Survivor-46-Q-Burdette.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Tim",
 					"image": "./assets/Survivor-46-Tim-Spicer.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Jem",
 					"image": "./assets/Survivor-46-Jemila-Jem-Hussain-Adams.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 				{
 					"name": "Moriah",
 					"image": "./assets/Survivor-46-Moriah-Gaynor.jpg",
-					"eliminated": false
+					"eliminated": false,
+					"hasIdol": false,
+					"hasAdvantage": false
 				},
 			]
 		},
@@ -107,7 +139,11 @@ document.addEventListener("DOMContentLoaded", function () {
 		return `
 				<div class="player">
 					<img src="${player.image}" alt="${player.name}">
-					<span>${player.name}</span>
+					<span>
+						${player.name} 
+						${player.hasIdol ? '<div class="material-symbols-outlined">verified_user</div>' : ''}
+						${player.hasAdvantage ? '<div class="material-symbols-outlined">upgrade</div>' : ''}
+						</span>
 					${player.eliminated ? '<span class="eliminated">X</span>' : ''}
 				</div>	
 			`;
